eacc9721c41b909975fb0919e65505033c5244b8
jasester <jasester@163.com>
2018-11-28 16:42:09 +0800

    update GeetestTool

--- src/GeetestTool.php
+++ src/GeetestTool.php
@@ -9,6 +9,8 @@ class GeetestTool
 {
     protected $GtSdk;
 
+    protected $minutes = 15; //默认60分钟
+
     public function __construct()
     {
         $this->GtSdk = new GeetestLib(config('geetest.captcha_id'), config('geetest.captcha_key'));
@@ -26,8 +28,12 @@ class GeetestTool
             'ip_address' => (array_key_exists('ip_address', $param)) ? $param['ip_address'] : '127.0.0.1', # 请在此处传输用户请求验证时所携带的IP
         ];
         $status = $this->GtSdk->pre_process($data, 1);
-        $_SESSION['gtserver'] = $status;
-        $_SESSION['user_id'] = $data['user_id'];
+        // $_SESSION['gtserver'] = $status;
+        // $_SESSION['user_id'] = $data['user_id'];
+        // $_SESSION['ip_address'] = $data['ip_address'];
+        cache('gtserver', $status, $this->minutes);
+        cache('user_id', $data['user_id'], $this->minutes);
+        cache('ip_address', $data['ip_address'], $this->minutes);
         echo $this->GtSdk->get_response_str();
     }
 
@@ -37,10 +43,15 @@ class GeetestTool
      */
     public function VerifyCaptchaServlet ()
     {
+        // $data = array(
+        //     "user_id" => $_SESSION['user_id'], # 网站用户id
+        //     "client_type" => config('geetest.client_type'), #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+        //     "ip_address" => $_SESSION['ip_address'] # 请在此处传输用户请求验证时所携带的IP
+        // );
         $data = array(
-            "user_id" => $_SESSION['user_id'], # 网站用户id
+            "user_id" => cache('user_id'), # 网站用户id
             "client_type" => config('geetest.client_type'), #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-            "ip_address" => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
+            "ip_address" => cache('ip_address') # 请在此处传输用户请求验证时所携带的IP
         );
         if ($_SESSION['gtserver'] == 1) {   //服务器正常
             $result = $this->GtSdk->success_validate($_POST['geetest_challenge'], $_POST['geetest_validate'], $_POST['geetest_seccode'], $data);
