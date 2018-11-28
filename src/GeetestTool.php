19b41ea9e53bad22e34193c2568e50e1d0f461a4
jasester <jasester@163.com>
2018-11-28 16:56:51 +0800

    Update GeetestTool session

--- src/GeetestTool.php
+++ src/GeetestTool.php
@@ -28,9 +28,6 @@ class GeetestTool
             'ip_address' => (array_key_exists('ip_address', $param)) ? $param['ip_address'] : '127.0.0.1', # 请在此处传输用户请求验证时所携带的IP
         ];
         $status = $this->GtSdk->pre_process($data, 1);
-        // $_SESSION['gtserver'] = $status;
-        // $_SESSION['user_id'] = $data['user_id'];
-        // $_SESSION['ip_address'] = $data['ip_address'];
         cache('gtserver', $status, $this->minutes);
         cache('user_id', $data['user_id'], $this->minutes);
         cache('ip_address', $data['ip_address'], $this->minutes);
@@ -41,27 +38,22 @@ class GeetestTool
      * 验证
      * @return bool
      */
-    public function VerifyCaptchaServlet ()
+    public function VerifyCaptchaServlet (array $param = [])
     {
-        // $data = array(
-        //     "user_id" => $_SESSION['user_id'], # 网站用户id
-        //     "client_type" => config('geetest.client_type'), #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-        //     "ip_address" => $_SESSION['ip_address'] # 请在此处传输用户请求验证时所携带的IP
-        // );
         $data = array(
             "user_id" => cache('user_id'), # 网站用户id
             "client_type" => config('geetest.client_type'), #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
             "ip_address" => cache('ip_address') # 请在此处传输用户请求验证时所携带的IP
         );
-        if ($_SESSION['gtserver'] == 1) {   //服务器正常
-            $result = $this->GtSdk->success_validate($_POST['geetest_challenge'], $_POST['geetest_validate'], $_POST['geetest_seccode'], $data);
+        if (cache('gtserver') == 1) {   //服务器正常
+            $result = $this->GtSdk->success_validate($param['geetest_challenge'], $param['geetest_validate'], $param['geetest_seccode'], $data);
             if ($result) {
                 return true;
             } else{
                 return false;
             }
         }else{  //服务器宕机,走failback模式
-            if ($this->GtSdk->fail_validate($_POST['geetest_challenge'],$_POST['geetest_validate'],$_POST['geetest_seccode'])) {
+            if ($this->GtSdk->fail_validate($param['geetest_challenge'],$param['geetest_validate'],$param['geetest_seccode'])) {
                 return true;
             }else{
                 return false;
